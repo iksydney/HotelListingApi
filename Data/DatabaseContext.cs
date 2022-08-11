396a1c9e6e6de5c53a0f3c9e98256943653da12e
Ogbonna Ikenna Sydney <cindionline@yahoo.com>
2022-08-11 20:50:44 +0100

    Initial commit

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {}
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Nigeria",
+                    ShortName = "NIG"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "United States Of America",
+                    ShortName = "USA"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Ghana",
+                    ShortName = "GH"
+                },
+                new Country
+                {
+                    Id = 4,
+                    Name = "Canada",
+                    ShortName = "CAN"
+                }
+                );
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Address = "Terhemba Street",
+                    CountryId = 1,
+                    Rating = 4.5
+                }, new Hotel
+                {
+                    Id = 2,
+                    Address = "Olorunfunmi Street",
+                    CountryId = 2,
+                    Rating = 5.0
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Address = "Goddy Street",
+                    CountryId = 3,
+                    Rating = 3.8
+                }
+                );
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
---
+++ Models/CountryDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models
+{
+    public class CountryDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "Country name is too long")]
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
