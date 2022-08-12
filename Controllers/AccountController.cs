15ed6ca7b2d1b7cbe2bfd92de2c681e0682797a1
Ogbonna Ikenna Sydney <cindionline@yahoo.com>
2022-08-12 16:21:25 +0100

    Register functionality

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using HotelListing.Data;
+using HotelListing.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+        public AccountController(UserManager<User> usermanager, 
+            SignInManager<User> signInManager, 
+            IMapper mapper, 
+            ILogger logger)
+        {
+            _userManager = usermanager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+            _logger = logger;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+            _logger.LogInformation($"Registration attempt from{userDTO.Email}");
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                var user = _mapper.Map<User>(userDTO);
+                var result  = await _userManager.CreateAsync(user);
+
+                if (!result.Succeeded)
+                {
+                    return BadRequest("User registration attempt failed");
+                }
+
+            }catch(Exception ex)
+            {
+                _logger.LogError(ex, $"Something went wrong in the {nameof(Register)}");
+                return Problem($"Something went wrong in the {nameof(Register)}", statusCode: 500);
+            }
+
+        }
+
+    }
+}
---
+++ Models/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models
+{
+    public class LoginUserDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "Your Password is limited to 20 characters ")]
+        public string Password { get; set; }
+    }
+    public class UserDTO : LoginUserDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+    }
+}
